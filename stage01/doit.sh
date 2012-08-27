bf1a3711e6ab3780c5dd9b0018e117db3eb80ea6
Vic Iglesias <vic.iglesias@eucalyptus.com>
2012-08-27 12:48:33 -0700

    Change to precise image as default. Includes the following load in rc.local: stress --cpu 1 --io 1

--- stage01/doit.sh
+++ stage01/doit.sh
@@ -23,7 +23,7 @@ function download_and_setup_euca2ools {
 download_and_setup_euca2ools
 
 if [ $1 = "xen" ]; then
-./bundleit.sh ../share/euca-ubuntu-9.04-x86_64/xen-kernel/vmlinuz-2.6.27.21-0.1-xen ../share/euca-ubuntu-9.04-x86_64/xen-kernel/initrd-2.6.27.21-0.1-xen ../share/euca-ubuntu-9.04-x86_64/ubuntu.9-04.x86-64.img
+./bundleit.sh ../share/instance_images/precise-server/xen-kernel/vmlinuz-3.2.0-23-virtual ../share/instance_images/precise-server/xen-kernel/initrd.img-3.2.0-23-virtual ../share/instance_images/precise-server/precise-server-cloudimg-amd64-ext3.img
 else
-./bundleit.sh ../share/euca-ubuntu-9.04-x86_64/kvm-kernel/vmlinuz-2.6.28-11-generic ../share/euca-ubuntu-9.04-x86_64/kvm-kernel/initrd.img-2.6.28-11-generic ../share/euca-ubuntu-9.04-x86_64/ubuntu.9-04.x86-64.img
+./bundleit.sh ../share/instance_images/precise-server/kvm-kernel/vmlinuz-3.2.0-23-virtual ../share/instance_images/precise-server/kvm-kernel/initrd.img-3.2.0-23-virtual ../share/instance_images/precise-server/precise-server-cloudimg-amd64-ext3.img
 fi
